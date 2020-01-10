d1c0d2ab9a03254732fdd22fe4210f3aed723bf6
Shailendra <shailendra.singh@talentica.com>
2020-01-10 09:37:14 +0530

    added lesson4 - error detection examples

---
+++ lesson4.py
@@ -0,0 +1,39 @@
+# error detection
+
+# Runtime exception
+# NameError
+# NameError: name 'variable' is not defined
+'''
+vaariable = 55
+print(variable);
+'''
+
+# Runtime exception
+# Expected Indent
+# IndentationError: expected an indented block
+'''
+def function():
+print(2)
+'''
+
+# Unexpected Indent
+# IndentationError: unexpected indent
+'''
+print(2)
+    print(3)
+'''
+
+# Syntax error
+# EOL error
+# SyntaxError: EOL while scanning string literal
+# no ending of literal in print method
+'''
+print("23
+'''
+
+# syntax error
+# EOF error
+# SyntaxError: unexpected EOF while parsing
+'''
+print("hello"
+'''
