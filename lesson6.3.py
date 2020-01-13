98a8505aab597db4809c57d4ca1bb11678414f4f
Shailendra <shailendra.singh@talentica.com>
2020-01-13 23:15:38 +0530

    lesson6.3 - importing modules

---
+++ lesson6.3.py
@@ -0,0 +1,13 @@
+
+# importing modules
+# basically modules are nothing but file names
+# and we can import the content within the module(file)
+
+# usually we can import modules within
+# - same directory
+# - <pyhton localtion>/Lib
+
+# here classes is python file within same directory
+from classes import calculator as c
+
+print (c.mul(2,3))
