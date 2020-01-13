038bc6be5978391859eddff8593bb2ea78cd27dc
Shailendra <shailendra.singh@talentica.com>
2020-01-13 22:46:19 +0530

    lesson6 - added input, statistics related methods

---
+++ lesson6.py
@@ -0,0 +1,19 @@
+# input in python3
+name = input("name: ")
+print("Hello", name)
+
+# statistics
+import statistics
+
+ll = [1,2,3,4,5]
+
+# mean
+print ("mean:", statistics.mean(ll))
+# meadian
+print ("median:",statistics.median(ll))
+# mode
+print ("mode:",statistics.mode(ll))
+# std deviation
+print ("std. deviation:", statistics.stdev(ll))
+# variance
+print ("variance:",statistics.variance(ll))
